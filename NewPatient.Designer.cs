2dee54c2fcc6c64b42c6ed9802934ef661bac2c7
PatelVishakha-07 <vishakhapatel8270>
2024-12-07 11:19:00 +0530

    medicine backend code added

---
+++ NewPatient.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Clinic_Management_System
+{
+    partial class NewPatient
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            btnSearch = new Button();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            txtSearch = new TextBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(192, 192, 255);
+            panel1.Controls.Add(btnSearch);
+            panel1.Controls.Add(dataGridView1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(txtSearch);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(835, 725);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            btnSearch.Location = new Point(649, 112);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(145, 41);
+            btnSearch.TabIndex = 5;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            btnSearch.Click += btnSearch_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(13, 206);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(781, 482);
+            dataGridView1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+            label1.Location = new Point(340, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(170, 35);
+            label1.TabIndex = 3;
+            label1.Text = "New Patients";
+            // 
+            // txtSearch
+            // 
+            txtSearch.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            txtSearch.Location = new Point(13, 112);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(588, 41);
+            txtSearch.TabIndex = 1;
+            // 
+            // NewPatient
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panel1);
+            Name = "NewPatient";
+            Size = new Size(835, 725);
+            Load += NewPatient_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TextBox txtSearch;
+        private Label label1;
+        private DataGridView dataGridView1;
+        private Button btnSearch;
+    }
+}
--- Patients.Designer.cs
+++ Patients.Designer.cs
@@ -34,9 +34,9 @@
             menuStrip1 = new MenuStrip();
             showPatientsToolStripMenuItem = new ToolStripMenuItem();
             addPatientsToolStripMenuItem = new ToolStripMenuItem();
-            PanelLeft = new Panel();
             neewPatientsToolStripMenuItem = new ToolStripMenuItem();
             oldPatientsToolStripMenuItem = new ToolStripMenuItem();
+            PanelLeft = new Panel();
             PatientsPanel.SuspendLayout();
             PanelTop.SuspendLayout();
             menuStrip1.SuspendLayout();
@@ -50,7 +50,7 @@
             PatientsPanel.Dock = DockStyle.Fill;
             PatientsPanel.Location = new Point(0, 0);
             PatientsPanel.Name = "PatientsPanel";
-            PatientsPanel.Size = new Size(990, 760);
+            PatientsPanel.Size = new Size(1082, 716);
             PatientsPanel.TabIndex = 0;
             // 
             // MainPanel
@@ -58,7 +58,7 @@
             MainPanel.Dock = DockStyle.Fill;
             MainPanel.Location = new Point(247, 62);
             MainPanel.Name = "MainPanel";
-            MainPanel.Size = new Size(743, 698);
+            MainPanel.Size = new Size(835, 654);
             MainPanel.TabIndex = 2;
             // 
             // PanelTop
@@ -67,7 +67,7 @@
             PanelTop.Dock = DockStyle.Top;
             PanelTop.Location = new Point(247, 0);
             PanelTop.Name = "PanelTop";
-            PanelTop.Size = new Size(743, 62);
+            PanelTop.Size = new Size(835, 62);
             PanelTop.TabIndex = 1;
             // 
             // menuStrip1
@@ -80,7 +80,7 @@
             menuStrip1.Items.AddRange(new ToolStripItem[] { showPatientsToolStripMenuItem, addPatientsToolStripMenuItem, neewPatientsToolStripMenuItem, oldPatientsToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(743, 62);
+            menuStrip1.Size = new Size(835, 62);
             menuStrip1.TabIndex = 1;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -100,32 +100,34 @@
             addPatientsToolStripMenuItem.Text = "Add Patients";
             addPatientsToolStripMenuItem.Click += addPatientsToolStripMenuItem_Click;
             // 
-            // PanelLeft
-            // 
-            PanelLeft.Dock = DockStyle.Left;
-            PanelLeft.Location = new Point(0, 0);
-            PanelLeft.Name = "PanelLeft";
-            PanelLeft.Size = new Size(247, 760);
-            PanelLeft.TabIndex = 0;
-            // 
             // neewPatientsToolStripMenuItem
             // 
             neewPatientsToolStripMenuItem.Name = "neewPatientsToolStripMenuItem";
             neewPatientsToolStripMenuItem.Size = new Size(170, 58);
             neewPatientsToolStripMenuItem.Text = "New Patients";
+            neewPatientsToolStripMenuItem.Click += neewPatientsToolStripMenuItem_Click;
             // 
             // oldPatientsToolStripMenuItem
             // 
             oldPatientsToolStripMenuItem.Name = "oldPatientsToolStripMenuItem";
             oldPatientsToolStripMenuItem.Size = new Size(170, 58);
             oldPatientsToolStripMenuItem.Text = "Old Patients";
+            oldPatientsToolStripMenuItem.Click += oldPatientsToolStripMenuItem_Click;
+            // 
+            // PanelLeft
+            // 
+            PanelLeft.Dock = DockStyle.Left;
+            PanelLeft.Location = new Point(0, 0);
+            PanelLeft.Name = "PanelLeft";
+            PanelLeft.Size = new Size(247, 716);
+            PanelLeft.TabIndex = 0;
             // 
             // Patients
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.LightGray;
-            ClientSize = new Size(990, 760);
+            ClientSize = new Size(1082, 716);
             Controls.Add(PatientsPanel);
             Name = "Patients";
             StartPosition = FormStartPosition.CenterScreen;
