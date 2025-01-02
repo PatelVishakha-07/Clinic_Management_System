0e4b06381cacfde95afaf31480fea67efc6e06b0
saima <saimashaikh0231@gmail.com>
2025-01-02 11:08:01 +0530

    w

--- ShowMedicine.cs
+++ ShowMedicine.cs
@@ -14,12 +14,12 @@ namespace Clinic_Management_System
     {
         databaseclass dbClass = new databaseclass();
         string str;
+
         public ShowMedicine(string s)
         {
             InitializeComponent();
             str = s;
         }
-
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
@@ -27,98 +27,100 @@ namespace Clinic_Management_System
             {
                 DateTime date = DateTime.Now;
                 string formattedDate = date.ToString("yyyy-MM-dd");
-                string query = "select m.medicine_id, m.medicine_name, m.company_name, m.medicine_type, md.medicine_stock," +
-                    " md.expiry_date, md.purchase_price, md.sell_price from Medicines m join Medicine_Details md on " +
-                    "m.medicine_id = md.medicine_id where md.expiry_date > '" + formattedDate + "' and CAST(md.medicine_stock AS INTEGER) > 0 order by medicine_name;";
+                string query = "SELECT m.medicine_id, m.medicine_name, m.company_name, m.medicine_type, md.medicine_stock, " +
+                               "md.expiry_date, md.purchase_price, md.sell_price " +
+                               "FROM Medicines m JOIN Medicine_Details md ON m.medicine_id = md.medicine_id " +
+                               "WHERE md.expiry_date > '" + formattedDate + "' AND CAST(md.medicine_stock AS INTEGER) > 0 " +
+                               "ORDER BY medicine_name;";
                 DataSet ds = dbClass.Getdata(query);
                 populategridview(ds);
             }
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)        {}
-
-        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)        {}
-
-        private void btnSearch_Click(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
             string value = txtSearch.Text;
             string query;
+
             if (!string.IsNullOrEmpty(value))
             {
-
+                // Build the query based on the input
                 if (int.TryParse(value, out int stock))
                 {
-                    query = $"select m.Medicine_Name, m.Company_Name, m.Medicine_Type, md.Medicine_Stock, md.Expiry_Date, md.purchase_price" +
-                        $", md.sell_price from Medicines m join medicine_details md on m.medicine_id = md.medicine_id" +
-                        $" where md.Medicine_Stock={int.Parse(value)};";
+                    query = $"SELECT m.Medicine_Id, m.Medicine_Name, m.Company_Name, m.Medicine_Type, md.Medicine_Stock, md.Expiry_Date, md.Purchase_Price, md.Sell_Price " +
+                            $"FROM Medicines m JOIN Medicine_Details md ON m.medicine_id = md.medicine_id " +
+                            $"WHERE md.Medicine_Stock = {stock};";
                 }
-                else if (DateOnly.TryParse(value, out DateOnly expiry_date))
+                else if (DateOnly.TryParse(value, out DateOnly expiryDate))
                 {
-                    string formated_date = expiry_date.ToString("MM-dd-yyyy");
-                    query = $"select m.Medicine_Name, m.Company_Name, m.Medicine_Type, md.Medicine_Stock, md.Expiry_Date, md.purchase_price, " +
-                        $" md.sell_price from Medicines m join medicine_details md on m.medicine_id = md.medicine_id " +
-                        $"where md.Expiry_Date='{formated_date}';";
+                    string formattedDate = expiryDate.ToString("yyyy-MM-dd");
+                    query = $"SELECT m.Medicine_Id, m.Medicine_Name, m.Company_Name, m.Medicine_Type, md.Medicine_Stock, md.Expiry_Date, md.Purchase_Price, md.Sell_Price " +
+                            $"FROM Medicines m JOIN Medicine_Details md ON m.medicine_id = md.medicine_id " +
+                            $"WHERE md.Expiry_Date = '{formattedDate}';";
                 }
                 else
                 {
-                    query = $"select m.Medicine_Name, m.Company_Name, m.Medicine_Type, md.Medicine_Stock, md.Expiry_Date, md.purchase_price, " +
-                        $" md.sell_price from Medicines m join medicine_details md on m.medicine_id = md.medicine_id " +
-                        $"where Medicine_Name='{value}' or Company_Name='{value}';";
-
+                    query = $"SELECT m.Medicine_Id, m.Medicine_Name, m.Company_Name, m.Medicine_Type, md.Medicine_Stock, md.Expiry_Date, md.Purchase_Price, md.Sell_Price " +
+                            $"FROM Medicines m JOIN Medicine_Details md ON m.medicine_id = md.medicine_id " +
+                            $"WHERE m.Medicine_Name LIKE '%{value}%' OR m.Company_Name LIKE '%{value}%';";
                 }
-
             }
             else
             {
-                query = "select m.Medicine_Name, m.Company_Name, m.Medicine_Type, md.Medicine_Stock, md.Expiry_Date, md.purchase_price, " +
-                    "md.sell_price from Medicines m join medicine_details md on m.medicine_id = md.medicine_id ;";
+                // If the input is empty, fetch all records
+                query = "SELECT m.Medicine_Id, m.Medicine_Name, m.Company_Name, m.Medicine_Type, md.Medicine_Stock, md.Expiry_Date, md.Purchase_Price, md.Sell_Price " +
+                        "FROM Medicines m JOIN Medicine_Details md ON m.medicine_id = md.medicine_id;";
             }
+
+            // Fetch and populate the data
             DataSet ds = dbClass.Getdata(query);
             populategridview(ds);
         }
 
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            // Manually trigger the search on button click (optional)
+            textBox1_TextChanged(sender, e);
+        }
+
         private void populategridview(DataSet ds)
         {
-            
-                if (ds != null && ds.Tables.Count > 0)
+            if (ds != null && ds.Tables.Count > 0)
+            {
+                medicinegrid.AutoGenerateColumns = false;
+                medicinegrid.Columns["medicine_name"].DataPropertyName = "medicine_name";
+                medicinegrid.Columns["company_name"].DataPropertyName = "company_name";
+                medicinegrid.Columns["medicine_type"].DataPropertyName = "medicine_type";
+                medicinegrid.Columns["medicine_stock"].DataPropertyName = "medicine_stock";
+                medicinegrid.Columns["expiry_date"].DataPropertyName = "expiry_date";
+                medicinegrid.Columns["purchase_price"].DataPropertyName = "purchase_price";
+                medicinegrid.Columns["sell_price"].DataPropertyName = "sell_price";
+                medicinegrid.DataSource = ds.Tables[0];
+
+                if (!medicinegrid.Columns.Contains("Medicine_Id"))
                 {
-                    medicinegrid.AutoGenerateColumns = false;
-                    medicinegrid.Columns["medicine_name"].DataPropertyName = "medicine_name";
-                    medicinegrid.Columns["company_name"].DataPropertyName = "company_name";
-                    medicinegrid.Columns["medicine_type"].DataPropertyName = "medicine_type";
-                    medicinegrid.Columns["medicine_stock"].DataPropertyName = "medicine_stock";
-                    medicinegrid.Columns["expiry_date"].DataPropertyName = "expiry_date";
-                    medicinegrid.Columns["purchase_price"].DataPropertyName = "purchase_price";
-                    medicinegrid.Columns["sell_price"].DataPropertyName = "sell_price";
-                    medicinegrid.DataSource = ds.Tables[0];
-
-                    if (!medicinegrid.Columns.Contains("Medicine_Id"))
+                    var idColumn = new DataGridViewTextBoxColumn
                     {
-                        var idColumn = new DataGridViewTextBoxColumn
-                        {
-                            Name = "Medicine_Id",
-                            DataPropertyName = "Medicine_Id",
-                            Visible = false
-                        };
-                        medicinegrid.Columns.Add(idColumn);
-                    }
-
-                    if (!medicinegrid.Columns.Contains("updateLink") && str == "Doctor")
-                    {
-                        DataGridViewLinkColumn updateLink = new DataGridViewLinkColumn
-                        {
-                            HeaderText = "Action",
-                            Name = "updateLink",
-                            Text = "Update",
-                            UseColumnTextForLinkValue = true,
-                        };
-
-                        medicinegrid.Columns.Add(updateLink);
-                    }
-
+                        Name = "Medicine_Id",
+                        DataPropertyName = "Medicine_Id",
+                        Visible = false
+                    };
+                    medicinegrid.Columns.Add(idColumn);
                 }
-            
-            
+
+                if (!medicinegrid.Columns.Contains("updateLink") && str == "Doctor")
+                {
+                    DataGridViewLinkColumn updateLink = new DataGridViewLinkColumn
+                    {
+                        HeaderText = "Action",
+                        Name = "updateLink",
+                        Text = "Update",
+                        UseColumnTextForLinkValue = true,
+                    };
+
+                    medicinegrid.Columns.Add(updateLink);
+                }
+            }
         }
 
         private void medicinegrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -127,16 +129,16 @@ namespace Clinic_Management_System
             {
                 if (e.ColumnIndex == medicinegrid.Columns["updateLink"].Index && e.RowIndex >= 0)
                 {
-                    int medicineId = Convert.ToInt32(medicinegrid.Rows[e.RowIndex].Cells["Medicine_Id"].Value.ToString());
-                    string medicineName = medicinegrid.Rows[e.RowIndex].Cells["Medicine_Name"].Value.ToString();
-                    string cmpName = medicinegrid.Rows[e.RowIndex].Cells["Company_Name"].Value.ToString();
-                    string medType = medicinegrid.Rows[e.RowIndex].Cells["Medicine_Type"].Value.ToString();
-                    int stock = Convert.ToInt32(medicinegrid.Rows[e.RowIndex].Cells["Medicine_Stock"].Value.ToString());
-                    string date = medicinegrid.Rows[e.RowIndex].Cells["Expiry_Date"].Value.ToString();
+                    // Ensure the Medicine_Id column is accessible for the update action
+                    int medicineId = Convert.ToInt32(medicinegrid.Rows[e.RowIndex].Cells["Medicine_Id"].Value);
+                    string medicineName = medicinegrid.Rows[e.RowIndex].Cells["medicine_name"].Value.ToString();
+                    string cmpName = medicinegrid.Rows[e.RowIndex].Cells["company_name"].Value.ToString();
+                    string medType = medicinegrid.Rows[e.RowIndex].Cells["medicine_type"].Value.ToString();
+                    int stock = Convert.ToInt32(medicinegrid.Rows[e.RowIndex].Cells["medicine_stock"].Value);
+                    string date = medicinegrid.Rows[e.RowIndex].Cells["expiry_date"].Value.ToString();
                     string purchase = medicinegrid.Rows[e.RowIndex].Cells["purchase_price"].Value.ToString();
                     string sell = medicinegrid.Rows[e.RowIndex].Cells["sell_price"].Value.ToString();
 
-                    //int medicineId = 0;
                     UpdateMedicine updateMedicine = new UpdateMedicine();
                     updateMedicine.getMedicineDetails(medicineId, medicineName, medType, cmpName, stock, date, purchase, sell);
                     Medicine medicine = this.FindForm() as Medicine;
@@ -149,6 +151,6 @@ namespace Clinic_Management_System
             }
         }
 
-        private void label1_Click(object sender, EventArgs e)        {        }
+        private void label1_Click(object sender, EventArgs e) { }
     }
 }
